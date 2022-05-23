9a5b6b8bcce848183dbfd285db55a3030d5824cb
Aishwarya125 <105542476+Aishwarya125@users.noreply.github.com>
2022-05-23 18:25:51 +0530

    Update Bike.java

--- src/org/bike/Bike.java
+++ src/org/bike/Bike.java
@@ -7,6 +7,9 @@ System.out.println("cost :90000");
 }public void speed() {
 	// TODO Auto-generated method stub
 System.out.println("60km per hr");
+	private void location() {
+System.out.println("Chennai");
+}
 }public static void main(String[] args) {
 	Bike b=new Bike();
 	b.cost();
